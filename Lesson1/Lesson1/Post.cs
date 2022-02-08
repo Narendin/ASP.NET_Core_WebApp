bd1539ffd9b094f391a8ac78a0a115538df13e36
VladimirDolguev <avus93@mail.ru>
2022-02-08 19:02:35 +0300

    Урок 1.
    Асинхронное получение постов и запись в файл.

---
+++ Lesson1/Lesson1/Post.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Lesson1
+{
+    public class Post
+    {
+        public Post(string json)
+        {
+            JToken jObject = JObject.Parse(json);
+            UserId = (int)jObject["userId"];
+            Id = (int)jObject["id"];
+            Title = (string)jObject["title"];
+            Body = jObject["body"].ToString().Replace("\n", "");
+        }
+
+        public int UserId { get; private set; }
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Body { get; private set; }
+
+        public override string ToString()
+        {
+            return UserId + "\n" + Id + "\n" + Title + "\n" + Body + "\n";
+        }
+    }
+}
---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var tasks = new List<Task<string>>();
+            for (var i = 4; i <= 13; i++)
+            {
+                tasks.Add(GetPostAsync(i));
+            }
+
+            var results = Task.WhenAll(tasks);
+
+            var fs = new FileStream("results.txt", FileMode.OpenOrCreate);
+
+            using (var sw = new StreamWriter(fs))
+            {
+                foreach (var result in results.Result)
+                {
+                    sw.WriteLine(new Post(result));
+                }
+            }
+
+            Console.WriteLine("Посты сохранены в файле result.txt");
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу...");
+            Console.ReadKey(true);
+        }
+
+        private static async Task<string> GetPostAsync(int postNum)
+        {
+            var client = new HttpClient();
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(10000);
+            var uri = new Uri($"https://jsonplaceholder.typicode.com/posts/{postNum}");
+
+            return await client.GetStringAsync(uri, cts.Token);
+        }
+    }
+}
